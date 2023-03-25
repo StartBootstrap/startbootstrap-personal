c0754258141c09565157028218a5fdc3b78b39bf
David Miller <hello@davidmiller.io>
2023-03-25 18:06:34 -0400

    Bootstrap 5.2.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Personal v1.0.0 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
 // This file is intentionally blank
